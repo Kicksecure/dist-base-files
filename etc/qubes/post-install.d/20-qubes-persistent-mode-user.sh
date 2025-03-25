252eb9731720e22941a3c6cb19275962f9893a30
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-03-25 17:36:36 -0500

    Only request boot mode features on Qubes R4.3

--- etc/qubes/post-install.d/20-qubes-persistent-mode-user.sh
+++ etc/qubes/post-install.d/20-qubes-persistent-mode-user.sh
@@ -3,7 +3,10 @@
 ## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-qvm-features-request boot-mode.kernelopts.user=''
-qvm-features-request boot-mode.name.user='PERSISTENT mode USER'
-qvm-features-request boot-mode.active='user'
-qvm-features-request boot-mode.appvm-default='user'
+qubes_version="$(cat /usr/share/qubes/marker-vm | grep -v '^#')"
+if echo "${qubes_version}" | grep -q '^4.3' ; then
+   qvm-features-request boot-mode.kernelopts.user=''
+   qvm-features-request boot-mode.name.user='PERSISTENT mode USER'
+   qvm-features-request boot-mode.active='user'
+   qvm-features-request boot-mode.appvm-default='user'
+fi
