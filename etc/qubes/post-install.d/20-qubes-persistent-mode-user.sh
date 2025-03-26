2b7ccb21016dab32d6668f6ba0930137c3111382
Patrick Schleizer <adrelanos@whonix.org>
2025-03-26 05:08:25 -0400

    future-proof for later Qubes versions

--- etc/qubes/post-install.d/20-qubes-persistent-mode-user.sh
+++ etc/qubes/post-install.d/20-qubes-persistent-mode-user.sh
@@ -3,8 +3,11 @@
 ## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
+## TODO: Qubes R4.3 and above: Once Qubes R4.2 support has been deprecated,
+##       the following 'if' clause should be removed.
+
 qubes_version="$(cat /usr/share/qubes/marker-vm | grep -v '^#')"
-if echo "${qubes_version}" | grep -q '^4.3' ; then
+if printf "%s\n" "${qubes_version}" | grep -q -e '^4.3' -e '^4.4' -e '^4.5' -e '^4.6' ; then
    qvm-features-request boot-mode.kernelopts.user=''
    qvm-features-request boot-mode.name.user='PERSISTENT mode USER'
    qvm-features-request boot-mode.active='user'
